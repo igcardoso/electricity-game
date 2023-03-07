80d73a759752bd2a7836a9f57ad89bb0713eab13
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-07 08:18:22 -0300

    alterção da posição do loading na pagina home

--- js/auth.js
+++ js/auth.js
@@ -29,7 +29,7 @@ submit.addEventListener("click", function() {
     var timer = setTimeout(function() {
       document.querySelector('#page').style.display = "flex";
       document.querySelector('#login').style.display = "none";
-      loading.style.left = "-100000px"
+      loading.style.left = "-100%"
     }, 3000);
   } else {
     window.alert("Usuario não encontrado");
