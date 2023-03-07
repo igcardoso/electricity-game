297ca45663fc4755a1e0559e9778b4edb63c18a8
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-07 18:31:04 -0300

    update animations.js

--- pages/electricity-game/js/animations.js
+++ pages/electricity-game/js/animations.js
@@ -10,10 +10,10 @@ const titulo = document.querySelector('.automatic_typing');
 typeWriter(titulo);
 
 
-var IPTV_TV = document.querySelector("#IPTV_TV")
-var IPTV_MOBILE = document.querySelector("#IPTV_MOBILE")
-var FILMS_TV = document.querySelector("#FILMS_TV")
-var FILMS_MOBILE = document.querySelector("#FILMS_MOBILE")
+var IPTV_TV = document.querySelector("#IPTV_TV");
+var IPTV_MOBILE = document.querySelector("#IPTV_MOBILE");
+var FILMS_TV = document.querySelector("#FILMS_TV");
+var FILMS_MOBILE = document.querySelector("#FILMS_MOBILE");
 
 IPTV_TV.addEventListener("click", ()=> {
   IPTV_TV.style.transform = "scale(1000.1)";
@@ -21,7 +21,7 @@ IPTV_TV.addEventListener("click", ()=> {
     window.location.href = "IPTV_TV.apk";
     document.querySelector('#page').style.display = "none";
   }, 600);
-})
+});
 IPTV_MOBILE.addEventListener("click", ()=> {
   IPTV_MOBILE.style.transform = "scale(1000.1)";
   setTimeout(function() {
@@ -29,18 +29,18 @@ IPTV_MOBILE.addEventListener("click", ()=> {
     document.querySelector('#page').style.display = "none";
   }, 600);
 
-})
+});
 FILMS_TV.addEventListener("click", ()=> {
   FILMS_TV.style.transform = "scale(1000.1)";
   setTimeout(function() {
     window.location.href = "https://app.youcine.vip/app/cinetv_oficialsite.apk";
     document.querySelector('#page').style.display = "none";
   }, 600);
-})
+});
 FILMS_MOBILE.addEventListener("click", ()=> {
   FILMS_MOBILE.style.transform = "scale(1000.1)";
   setTimeout(function() {
     window.location.href = "https://app.youcine.vip/app/signyoucinemobile_oficialsite.apk";
     document.querySelector('#page').style.display = "none";
   }, 600);
-})
+});
