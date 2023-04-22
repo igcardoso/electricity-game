518e6262df1a3b31ae6890bc99aeb2bd9afdfd70
igor <85964276+igcardoso@users.noreply.github.com>
2023-04-22 11:56:57 -0300

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,7 +9,8 @@ const assets =  [
   "/media/banner.png",
   "/media/banner_2.png",
   "/media/banner_3.png",
-  "/media/banner_4.png"
+  "/media/banner_4.png",
+  "/media/img_about_01.png"
 ];
 
 self.addEventListener("install", installEvent => {
